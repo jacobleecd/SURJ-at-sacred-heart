c74579d1209519be3b709e06e9bd508f1be92a9f
Jacob Coker-Dukowitz <jacobleecd@gmail.com>
2021-10-09 12:19:31 -0700

    Create matchMembersInAirTable.js

---
+++ matchMembersInAirTable.js
@@ -0,0 +1,128 @@
+// helper functions
+// TODO: comprhensive tests on this regex
+const validEmail = (email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+});
+
+// TODO: don't need to lowercase twice, don't need to check for non digits if we normalize
+const validPhone = (phone => {
+    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    return re.test(String(phone).toLowerCase());
+});
+
+const isUnmatchable = (({ email, phone, firstName }) => {
+    return firstName.length == 0 && (!validEmail(email) && !validPhone(phone));
+});
+
+// TODO: should we replace phone numbers and emails with noramlized phone numbers?
+const normalizePhone = (phone => {
+    return phone.replace(/[^\d+]+/g, '');
+});
+
+const phoneMatch = ((memberPhone, unmatchedPhone) =>  {
+    return validPhone(memberPhone) && validPhone(unmatchedPhone) && (memberPhone == unmatchedPhone)
+});
+
+const emailMatch = ((memberEmail, unmatchedEmail) => {
+    return validEmail(memberEmail) && validEmail(unmatchedEmail) && (memberEmail == unmatchedEmail)
+});
+
+const firstNameMatch = ((memberFirstName, unmatchedFirstName) => {
+    const smallestFirstNameLength = Math.min(memberFirstName.length, unmatchedFirstName.length)
+    const lengthToMatch = smallestFirstNameLength < 3 ? smallestFirstNameLength : 3
+    return memberFirstName.slice(0,lengthToMatch) == unmatchedFirstName.slice(0,lengthToMatch)
+});
+
+
+// constants
+const tablesWithUnmatchedRecords = [
+    {
+        id: "tbl4SUPc7SXFxZai0",
+        name: "Event Sign Up",
+        viewName: "unmatched_records",
+        firstNameField: "Form First Name",
+        lastNameField: "Form Last Name",
+        phoneField: "Form Phone",
+        emailField: "Form Email"
+    }
+];
+
+// TODO: validate fields in tables exist, raise error if not
+
+const membershipTable = { id: "tblzh5Ri7S1QnQ1t3", name: "Member List"};
+// TODO: cleanup - extract to object?
+const membershipTableFirstNameField = "First Name";
+const membershipTableLastNameField = "Last Name";
+const membershipTablePhoneFields = ["Primary Phone"];
+const membershipTableEmailFields = ["Primary Email"];
+const membershipTableFields = [membershipTableFirstNameField].concat(
+        membershipTableLastNameField, 
+        membershipTablePhoneFields,
+        membershipTableEmailFields);
+const existingMembersTable = base.getTable(membershipTable.id);
+const existingMembers = await existingMembersTable.selectRecordsAsync(
+    { fields: membershipTableFields }
+).then(queryResult => queryResult.records);;
+
+// for each table with unmatched records
+for (let i = 0; i < tablesWithUnmatchedRecords.length; i++) {
+    let table = tablesWithUnmatchedRecords[i];
+    let view = base.getTable(table.id).getView(table.viewName);
+    let unmatchedRecords = await view.selectRecordsAsync(
+        { fields: [
+                table.firstNameField, 
+                table.lastNameField, 
+                table.emailField, 
+                table.phoneField
+            ]
+        }
+    ).then(queryResult => queryResult.records);
+
+    // match records
+    for (let x = 0; x < unmatchedRecords.length; x++) {
+        let unmatchedRecord = unmatchedRecords[x];
+        let unmatchedFirstName = unmatchedRecord.getCellValue(table.firstNameField).toLowerCase();
+        // Last name is not currently used
+        // let unmatchedLastName = unmatchedRecord.getCellValue(table.lastNameField);
+        let unmatchedEmail = unmatchedRecord.getCellValue(table.emailField).toLowerCase();
+        let unmatchedPhone = normalizePhone(unmatchedRecord.getCellValue(table.phoneField));
+
+        if (isUnmatchable({ email: unmatchedEmail, phone: unmatchedPhone, firstName: unmatchedFirstName })) {
+            continue;
+        }
+
+        var match;
+        for (let y = 0; y < existingMembers.length; y++) {
+            let member = existingMembers[y];
+            let memberFirstName = member.getCellValue(table.firstNameField).toLowerCase();
+            // Last name is not currently used
+            // let memberLastName = member.getCellValue(table.lastNameField);
+            let memberEmail = member.getCellValue(table.emailField).toLowerCase();
+            let memberPhone = normalizePhone(member.getCellValue(table.phoneField));
+            
+            if (isUnmatchable({ email: memberEmail, phone: memberPhone, firstName: memberFirstName })) {
+                continue;
+            }
+
+            // let memberFields = { memberEmail, memberPhone, memberFirstName };
+            // let unamtchedMemberFields = { unmatchedEmail, unmatchedPhone, unmatchedFirstName };
+ 
+            if (phoneMatch({memberPhone, unmatchedPhone}) && emailMatch({ memberEmail, unmatchedEmail})) {
+                match = member; 
+                break;
+            } else if (phoneMatch({memberPhone, unmatchedPhone}) && firstNameMatch({memberFirstName, unmatchedFirstName})) {
+                match = member
+            } else if (!match && emailMatch({memberEmail, unmatchedEmail}) && firstNameMatch({memberFirstName, unmatchedFirstName})) {
+                match = member
+            }
+        };
+
+    if (!match) {
+        //      create a record in Memborship Table and assign to match
+        //         only input real phones & emails
+    }
+
+    // link the member record to match from Member List
+    }
+};
