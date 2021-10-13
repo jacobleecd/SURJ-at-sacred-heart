f305c952fdae411a6f81cc865f7048e1e4dd698d
Jacob Coker-Dukowitz <jacobleecd@gmail.com>
2021-10-12 19:49:36 -0700

    cleans up bugs, adds logging and more

--- matchMembersInAirTable.js
+++ matchMembersInAirTable.js
@@ -1,35 +1,35 @@
 // helper functions
 // TODO: comprhensive tests on this regex
 const validEmail = (email => {
+    if (!email) { return false; }
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 });
 
-// TODO: don't need to lowercase twice, don't need to check for non digits if we normalize
 const validPhone = (phone => {
+    if (!String(phone)) { return false; }
     const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
     return re.test(String(phone));
 });
 
 const isUnmatchable = (({ email, phone, firstName }) => {
-    return firstName.length == 0 && (!validEmail(email) && !validPhone(phone));
+    return !firstName || (!validEmail(email) && !validPhone(phone));
 });
 
-// TODO: should we replace phone numbers and emails with noramlized phone numbers?
-const normalizePhone = (phone => {
-    return phone.replace(/[^\d+]+/g, '');
-});
+const normalizePhone = (phone => !phone ? "" : String(phone).replace(/[^\d+]+/g, '') );
 
-const phoneMatch = ((memberPhone, unmatchedPhone) =>  {
+const normalizeText = (text => !text ? "" : String(text).toLowerCase() );
+
+const phoneMatch = (({ memberPhone, unmatchedPhone }) =>  {
     return validPhone(memberPhone) && validPhone(unmatchedPhone) && (memberPhone == unmatchedPhone)
 });
 
-const emailMatch = ((memberEmail, unmatchedEmail) => {
+const emailMatch = (({ memberEmail, unmatchedEmail }) => {
     return validEmail(memberEmail) && validEmail(unmatchedEmail) && (memberEmail == unmatchedEmail)
 });
 
-const firstNameMatch = ((memberFirstName, unmatchedFirstName) => {
-    if (unmatchedFirstName.length == 0 ) { return false; }
+const firstNameMatch = (( {memberFirstName, unmatchedFirstName }) => {
+    if (!memberFirstName || !unmatchedFirstName ) { return false; }
     const lengthToMatch = unmatchedFirstName.length < 3 ? unmatchedFirstName.length : 3
     return memberFirstName.slice(0,lengthToMatch) == unmatchedFirstName.slice(0,lengthToMatch)
 });
@@ -52,7 +52,7 @@ const tablesWithUnmatchedRecords = [
 // TODO: validate fields in tables exist, raise error if not
 // TODO: check if the updated_at field before trying to match to make sure this record is not in the process of being edited
 const membershipTable = { id: "tblzh5Ri7S1QnQ1t3", name: "Member List"};
-// TODO: cleanup - extract to object?
+// TODO: cleanup membershipTableFields - extract to object?
 const membershipTableFirstNameField = "First Name";
 const membershipTableLastNameField = "Last Name";
 const membershipTablePhoneFields = { primary: "Primary Phone" };
@@ -60,7 +60,7 @@ const membershipTableEmailFields = { primary: "Primary Email" };
 const membershipTableFields = [membershipTableFirstNameField].concat(
         membershipTableLastNameField, 
         membershipTablePhoneFields.primary,
-        membershipTableEmailFields.pimary);
+        membershipTableEmailFields.primary);
 const existingMembersTable = base.getTable(membershipTable.id);
 const existingMembers = await existingMembersTable.selectRecordsAsync(
     { fields: membershipTableFields }
@@ -80,64 +80,75 @@ for (let i = 0; i < tablesWithUnmatchedRecords.length; i++) {
         }
     ).then(queryResult => queryResult.records);
 
+    console.log(`Attempting to match ${unmatchedRecords.length} records from ${tableData.viewName} in ${tableData.name} table. `)
+    
+    let matchedRecords = 0;
+    let newRecords = 0;
+    let unmatchableRecords = 0;
     // for each unmatched record in table
     for (let x = 0; x < unmatchedRecords.length; x++) {
         let unmatchedRecord = unmatchedRecords[x];
-        let unmatchedFirstName = unmatchedRecord.getCellValue(tableData.firstNameField).toLowerCase();
-        // Last name is not currently used
-        let unmatchedLastName = unmatchedRecord.getCellValue(tableData.lastNameField);
-        let unmatchedEmail = unmatchedRecord.getCellValue(tableData.emailField).toLowerCase();
+        let unmatchedFirstName = normalizeText(unmatchedRecord.getCellValue(tableData.firstNameField));
+        let unmatchedEmail = normalizeText(unmatchedRecord.getCellValue(tableData.emailField));
         let unmatchedPhone = normalizePhone(unmatchedRecord.getCellValue(tableData.phoneField));
-
+        console.log("unmatchedPhone: ", unmatchedPhone)
         if (isUnmatchable({ email: unmatchedEmail, phone: unmatchedPhone, firstName: unmatchedFirstName })) {
+            unmatchableRecords += 1;
             continue;
         }
 
-        var match;
+        let match;
         // find a match in the Membership Table
         for (let y = 0; y < existingMembers.length; y++) {
             let member = existingMembers[y];
-            let memberFirstName = member.getCellValue(tableData.firstNameField).toLowerCase();
-            // Last name is not currently used
-            // let memberLastName = member.getCellValue(tableData.lastNameField);
-            let memberEmail = member.getCellValue(tableData.emailField).toLowerCase();
-            let memberPhone = normalizePhone(member.getCellValue(tableData.phoneField));
+            let memberFirstName = normalizeText(member.getCellValue(membershipTableFirstNameField));
+            let memberEmail = normalizeText(member.getCellValue(membershipTableEmailFields.primary));
+            let memberPhone = normalizePhone(member.getCellValue(membershipTablePhoneFields.primary));
             
             if (isUnmatchable({ email: memberEmail, phone: memberPhone, firstName: memberFirstName })) {
                 continue;
             }
- 
+
+            if (phoneMatch({memberPhone, unmatchedPhone})) {
+                console.log('phone match')
+            }
+
             if (phoneMatch({memberPhone, unmatchedPhone}) && emailMatch({ memberEmail, unmatchedEmail})) {
                 match = member; 
                 break;
             } else if (phoneMatch({memberPhone, unmatchedPhone}) && firstNameMatch({memberFirstName, unmatchedFirstName})) {
                 match = member
-            } else if (!match && emailMatch({memberEmail, unmatchedEmail}) && firstNameMatch({memberFirstName, unmatchedFirstName})) {
+            } else if (match == undefined && emailMatch({memberEmail, unmatchedEmail}) && firstNameMatch({memberFirstName, unmatchedFirstName})) {
                 match = member
             }
         };
 
         let matchId;
         // create the record if there is no match
-        if (!match) {
-            // TODO: only input real phones and emails
-            matchId = await existingMembersTable.createRecordAsync({
-                fields: {
-                    [membershipTableFirstNameField]: unmatchedFirstName,
-                    [membershipTableLastNameField]: unmatchedLastName,
-                    [membershipTableEmailFields.primary]: unmatchedEmail,
-                    [membershipTablePhoneFields.primary]: unmatchedPhone
-                }
-            });
+        if (match == undefined) {
+            let newMemberFields = {
+                    [membershipTableFirstNameField]: unmatchedRecord.getCellValue(tableData.firstNameField),
+                    [membershipTableLastNameField]: unmatchedRecord.getCellValue(tableData.lastNameField),
+            }
+
+            if (validEmail(unmatchedEmail))  { newMemberFields[membershipTableEmailFields.primary] = unmatchedEmail; }
+            if (validPhone(unmatchedPhone)) { newMemberFields[membershipTablePhoneFields.primary] = unmatchedPhone; }
+
+            matchId = await existingMembersTable.createRecordAsync(newMemberFields);
+            newRecords += 1;
         } else {
+            // TODO: also update existing record's information?
             matchId = match.id
+            matchedRecords += 1;
         }
+        
         // link the member record to match from Member List
-        // TODO: also update existing record's information?
         let table = base.getTable(tableData.id);
         await table.updateRecordAsync(unmatchedRecord.id, {
-            [tableData.memberLinkField]: matchId
+            [tableData.memberLinkField]: [{ id: matchId}]
         });
 
     }
+
+    console.log(`matched ${matchedRecords} with existing records and created ${newRecords} new records. There were ${unmatchableRecords} unmatchable records`)
 };
